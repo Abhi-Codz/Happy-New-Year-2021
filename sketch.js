bf5bc685b68cddb8077929afb88375b23d61196b
Abhinavprogrammer <71148737+Abhinavprogrammer@users.noreply.github.com>
2020-12-31 11:10:16 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function preload()
 {
     SantaChristmasImage = loadAnimation('ChristmasSanta1.png', 'ChristmasSanta2.png');
     NewYearImage = loadAnimation('2021-0.png', '2021-0.png', '2021-0.png', '2021-1.png', '2021-1.png', '2021-1.png', '2021-2.png', '2021-2.png', '2021-2.png', '2021-3.png', '2021-3.png', '2021-3.png', '2021-4.png', '2021-4.png', '2021-4.png', '2021-5.png', '2021-5.png', '2021-5.png', '2021-6.png', '2021-6.png', '2021-6.png', '2021-7.png', '2021-7.png', '2021-7.png', '2021-8.png', '2021-8.png', '2021-8.png', '2021-9.png', '2021-9.png', '2021-9.png', '2021-10.png', '2021-10.png', '2021-10.png', '2021-11.png', '2021-11.png', '2021-11.png', '2021-12.png', '2021-12.png', '2021-12.png', '2021-13.png', '2021-14.png', '2021-15.png', '2021-15.png', '2021-15.png', '2021-15.png', '2021-15.png');
-    song = loadSound('y2mate.com - Happy New Year 2020  Happy New Year Songs 2020  Top Happy New Year Songs Playlist 2020.mp3');
+    song = loadSound('y2mate.com - Happy New Year 2020  Happy New Year Songs 2020  Top Happy New Year Songs Playlist 2020-[AudioTrimmer.com].mp3');
     fireWorks = createImg('fireworks_gif.gif');
 }
 
